5b954a36df18bffc9e3b1fc94313f9f7e86baf54
Drukerroy <roy.druker95@gmail.com>
2023-07-15 20:23:21 +0300

    updated restapi

--- webapp/views/rest_api.py
+++ webapp/views/rest_api.py
@@ -7,20 +7,9 @@ import threading
 api = Blueprint("api", __name__, static_folder="static", template_folder="templates")
 
 
-@api.route("/")
-def index():
-    return render_template("index.html")
-
-
 @api.route("/run_vikings_scraper", methods=['GET', 'POST'])
 def run_vikings_scraper():
     thread = threading.Thread(target=run_scraper)
     thread.start()
     return {'scraper': 'running'}
 
-
-@api.route("/insert_entities_to_db", methods=['POST'])
-def insert_entities_to_db():
-    data = request.json
-    vikings_db.insert_entities(data)
-    return {'updated_db': 'true'}
